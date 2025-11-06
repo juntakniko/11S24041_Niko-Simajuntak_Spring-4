9086e7502d755270e13ce7d73e709050cd00e091
GideonLearnToSmile <242425103+juntakniko@users.noreply.github.com>
2025-11-06 20:43:18 +0700

    Sudah Selesai

--- README.md
+++ README.md
@@ -1,30 +1,18 @@
-# Spring v4.0 Proyek Starter
+# Spring v4.0 Starter Proyek
 
 ## Logs
-
-### 04-11-2025
-
-- Memperbarui kebutuhan paket
 
 ### 29-10-2025
 
 - Melakukan inisialisasi proyek
-
+- Menambahkan method `sayHello()` pada HomeController
+- Menambahkan pengujian untuk method `sayHello()`
 
 ## Syntax
 
 ### Melakukan Instal Ulang Kebutuhan Paket
 
 command: `mvn clean install`
-
-#### Windows: elakukan build ulang proyek dan membuka hasil laporan
-command with open jacoco: `mvn clean test; start target\site\jacoco\index.html`
-
-#### Mac: melakukan build ulang proyek dan membuka hasil laporan
-command with open jacoco: `mvn clean test && open target\site\jacoco\index.html`
-
-#### Linux: melakukan build ulang proyek dan membuka hasil laporan
-command with open jacoco: `mvn clean test && xdg-open target\site\jacoco\index.html`
 
 ### Menjalankan Aplikasi
 
@@ -34,14 +22,9 @@ URL: http://localhost:8080
 
 ### Menjalankan Test Covertage
 
-pre-command: `mvn clean install`
-
 command: `./mvnw test jacoco:report`
 
 command-check: `./mvnw clean test jacoco:check`
 
-## Purpose
-
-Proyek ini dibuat untuk tujuan **Pendidikan**.
 
 
--- src/test/java/org/delcom/starter/ApplicationTests.java
+++ src/test/java/org/delcom/starter/ApplicationTests.java
@@ -1,4 +1,4 @@
-package org.delcom.todos;
+package org.delcom.starter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.SpringApplication;
@@ -10,7 +10,7 @@ import static org.mockito.Mockito.*;
 class ApplicationTest {
 
 	@Test
-	void mainMethod_ShouldRunSpringApplication() throws Exception {
+	void mainMethod_ShouldRunSpringApplication() {
 		// Mock SpringApplication.run untuk test main method
 		try (var mockedSpring = mockStatic(SpringApplication.class)) {
 			ConfigurableApplicationContext mockContext = mock(ConfigurableApplicationContext.class);
@@ -26,24 +26,24 @@ class ApplicationTest {
 	}
 
 	@Test
-	void contextLoads_ShouldNotThrowException() throws Exception {
+	void contextLoads_ShouldNotThrowException() {
 		// Test bahwa Spring context bisa dimuat
 		assertDoesNotThrow(() -> {
 			// Test basic class loading
-			Class<?> clazz = Class.forName("org.delcom.todos.Application");
+			Class<?> clazz = Class.forName("org.delcom.starter.Application");
 			assertNotNull(clazz);
 		});
 	}
 
 	@Test
-	void todoApplication_ShouldHaveSpringBootAnnotation() throws Exception {
+	void todoApplication_ShouldHaveSpringBootAnnotation() {
 		// Test bahwa class memiliki annotation @SpringBootApplication
 		assertNotNull(Application.class
 				.getAnnotation(org.springframework.boot.autoconfigure.SpringBootApplication.class));
 	}
 
 	@Test
-	void todoApplication_CanBeInstantiated() throws Exception {
+	void todoApplication_CanBeInstantiated() {
 		// Test bahwa kita bisa membuat instance Application
 		assertDoesNotThrow(() -> {
 			Application app = new Application();
